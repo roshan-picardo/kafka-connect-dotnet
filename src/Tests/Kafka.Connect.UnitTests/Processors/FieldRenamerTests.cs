a2b77a8b086b228fce9cd4bddf13022a9d62a3c8
Picardo, Roshan Silvan <Roshan.Picardo@cba.com.au>
2022-08-31 23:03:38 +1000

    Adding FieldRenamer unit tests

--- src/Tests/Kafka.Connect.UnitTests/Processors/FieldRenamerTests.cs
+++ src/Tests/Kafka.Connect.UnitTests/Processors/FieldRenamerTests.cs
@@ -1,117 +1,70 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Kafka.Connect.Plugin.Models;
 using Kafka.Connect.Processors;
+using Microsoft.Extensions.Options;
+using NSubstitute;
 using Xunit;
 
-namespace Kafka.Connect.Tests.Processors
+namespace Kafka.Connect.UnitTests.Processors
 {
     public class FieldRenamerTests
     {
+        private readonly IOptions<List<ConnectorConfig<IDictionary<string, string>>>> _options;
         private readonly FieldRenamer _fieldRenamer;
-
         public FieldRenamerTests()
         {
-            _fieldRenamer = new FieldRenamer(null);
+            _options = Substitute.For<IOptions<List<ConnectorConfig<IDictionary<string, string>>>>>();
+            _fieldRenamer = new FieldRenamer(_options, Substitute.For<IOptions<ConnectorConfig<IDictionary<string, string>>>>());
+        }
+        
+        
+        
+        [Theory]
+        [InlineData(new []{ "simple.rename" }, new []{ "simple.rename:simple.new-name" }, new []{"simple.rename"}, "wrong-connector-name")]
+        [InlineData(new []{ "simple.rename" }, new []{ "simple.rename:simple.new-name" }, new []{"simple.rename"}, "connector-name", "Kafka.Connect.Processors.BlacklistFieldProjector")]
+        [InlineData(new []{ "simple.rename" }, new []{ "simple.rename:simple.new-name" }, new []{"simple.new-name"})]
+        [InlineData(new []{ "simple.rename", "simple.do-not" }, new []{"simple.rename:simple.new-name"}, new []{"simple.new-name", "simple.do-not"})]
+        [InlineData(new []{ "simple.do-not", "simple.rename.one", "simple.rename.two", "simple.rename.three.what" }, new []{"simple.rename.*:simple.new-name.*"}, new []{"simple.do-not", "simple.new-name.one", "simple.new-name.two", "simple.new-name.three.what"})]
+        [InlineData(new []{ "simple.do-not", "simple.one.rename", "simple.two.rename" }, new []{"simple.*.rename:simple.*.new-name"}, new []{"simple.do-not", "simple.one.new-name", "simple.two.new-name"})]
+        [InlineData(new []{ "simple.do-not", "one.simple.rename", "two.simple.rename" }, new []{"*.simple.rename:*.simple.new-name"}, new []{ "simple.do-not", "one.simple.new-name", "two.simple.new-name" })]
+        [InlineData(new []{ "simple.one.do-not.two.do-not", "simple.one.rename-one.two.rename-two", "simple.three.rename-one.four.rename-two", "simple.one.rename-one.two.do-not" }, new []{"simple.*.rename-one.*.rename-two:simple.*.new-name-first.*.new-name-second"}, new []{ "simple.one.do-not.two.do-not", "simple.one.new-name-first.two.new-name-second", "simple.three.new-name-first.four.new-name-second", "simple.one.rename-one.two.do-not"})]
+        [InlineData(new []{ "simple.list[0].item", "simple.list[1].item" }, new []{"simple.list[1].item:new-list[1].child"}, new []{"simple.list[0].item", "new-list[1].child"})]
+        [InlineData(new []{ "simple.list[0].item", "simple.list[1].item" }, new []{"simple.list[*].item:parent.push-down.new-list[*].child"}, new []{"parent.push-down.new-list[0].child", "parent.push-down.new-list[1].child"})]
+        [InlineData(new []{ "simple.list[0]", "simple.list[1]" }, new []{"simple.list[*]:renamed.new-list[*]"}, new []{"renamed.new-list[0]", "renamed.new-list[1]"})]
+        [InlineData(new []{ "simple.list[0].one.do-not", "simple.list[1].two.rename",  "simple.list[2].three.rename"  }, new []{"simple.list[*].*.rename:simple.list[*].*.new-name"}, new []{"simple.list[0].one.do-not","simple.list[1].two.new-name", "simple.list[2].three.new-name"})]
+        [InlineData(new []{ "simple.list[0].one.do-not", "simple.list[1].two.array[0].child.rename",  "simple.list[2].three.array[0].another.rename"  }, new []{"simple.list[*].*.array[*].*.rename:parent.list[*].*.child-list[*].*.new-name"}, new []{"simple.list[0].one.do-not", "parent.list[1].two.child-list[0].child.new-name", "parent.list[2].three.child-list[0].another.new-name"})]
+        [InlineData(new []{ "simple.list[0].one.do-not", "simple.list[1].two.array[0].child.rename",  "simple.list[2].three.array[0].another.item"  }, new []{"simple.list[1].*.array[*].*.rename:parent.list[1].*.child-list[*].*.new-name"}, new []{"simple.list[0].one.do-not", "parent.list[1].two.child-list[0].child.new-name", "simple.list[2].three.array[0].another.item"})]
+        public async Task Apply_Tests(string[] keys, string[] settings,  string[] expected, string connector = "connector-name", string processor = "Kafka.Connect.Processors.FieldRenamer")
+        {
+            foreach (var prefix in new[] {"key.", "value.", ""})
+            {
+                var options = new List<ConnectorConfig<IDictionary<string, string>>>
+                {
+                    new()
+                    {
+                        Name = connector,
+                        Processors = new List<ProcessorConfig<IDictionary<string, string>>>()
+                            {new() {Name = processor, Settings = settings.ToDictionary(s => $"{prefix}{s.Split(':')[0]}",
+                                s => $"{prefix}{s.Split(':')[1]}")}}
+                    }
+                };
+
+                var flattened = keys.ToDictionary(x => prefix == "" ? $"value.{x}" : $"{prefix}{x}", v => (object) "");
+
+                _options.Value.Returns(options);
+                var (skip, actual) =
+                    await _fieldRenamer.Apply(new Dictionary<string, object>(flattened), "connector-name");
+                Assert.False(skip);
+                Assert.Equal(actual.Count, expected.Length);
+                foreach (var (key, _) in actual)
+                {
+                    Assert.Contains(key,
+                        expected.Select(x => prefix == "" ? $"value.{x.Split(':')[0]}" : $"{prefix}{x.Split(':')[0]}"));
+                }
+            }
         }
 
-        [Fact]
-        public async Task Apply_Maps()
-        {
-            var (skip, flattened) = await _fieldRenamer.Apply(
-                new Dictionary<string, object>
-                {
-                    {"flat", "value1"}, 
-                    {"nested", "value1"},
-                    {"old.nested", "value"},
-                    {"key2", "value2"}}, 
-                @"maps: new Dictionary<string, string>()
-                {
-                    {""flat"", ""new""},
-                    {""nested"", ""new.nested""},
-                    {""old.nested"", ""makeFlat""},
-                }");
-            Assert.False(skip);
-            Assert.True(flattened.ContainsKey("value.new"));
-            Assert.False(flattened.ContainsKey("value.flat"));
-            
-            Assert.False(flattened.ContainsKey("value.nested"));
-            Assert.True(flattened.ContainsKey("value.new.nested"));
-            
-            Assert.False(flattened.ContainsKey("value.old.nested"));
-            Assert.True(flattened.ContainsKey("value.makeFlat"));
-            
-            Assert.True(flattened.ContainsKey("key2"));
-        }
-        
-        [Fact]
-        public async Task Apply_Maps_WildCard_Arrays()
-        {
-            var (skip, flattened) = await _fieldRenamer.Apply(
-                new Dictionary<string, object>
-                {
-                    {"key.array[0].first", "sample1"}, 
-                    {"key.array[1].first", "second"}}, 
-                @"maps: new Dictionary<string, string>()
-                {
-                    {""key.array[*].first"", ""new.array[*].renamed""}
-                }");
-            Assert.False(skip);
-            Assert.False(flattened.ContainsKey("key.array[0].first"));
-            Assert.False(flattened.ContainsKey("key.array[1].first"));
-            Assert.True(flattened.ContainsKey("value.new.array[0].renamed"));
-            Assert.True(flattened.ContainsKey("value.new.array[1].renamed"));
-        }
-        
-        [Fact]
-        public async Task Apply_Maps_WildCard_Elements()
-        {
-            var (skip, flattened) = await _fieldRenamer.Apply(
-                new Dictionary<string, object>
-                {
-                    {"end.wildcard.one", "wildcard"},
-                    {"end.wildcard.two", "wildcard"},
-                    
-                    {"wildcard.one.start", "wildcard"},
-                    {"wildcard.two.start", "wildcard"},
-                    
-                    {"start.one.end", "wildcard"},
-                    {"start.two.end", "wildcard"},
-                    }, 
-                @"maps: new Dictionary<string, string>()
-                {
-                    {""end.*"", ""renamed.*""},
-                    {""*.start"", ""*.renamed""},
-                    {""start.*.end"", ""renamed.*.renamed""}
-                }");
-            Assert.False(skip);
-            Assert.False(flattened.ContainsKey("end.wildcard.one"));
-            Assert.False(flattened.ContainsKey("end.wildcard.two"));
-            Assert.True(flattened.ContainsKey("value.renamed.wildcard.one"));
-            Assert.True(flattened.ContainsKey("value.renamed.wildcard.two"));
-            
-            Assert.False(flattened.ContainsKey("wildcard.one.start"));
-            Assert.False(flattened.ContainsKey("wildcard.one.start"));
-            Assert.True(flattened.ContainsKey("value.wildcard.one.renamed"));
-            Assert.True(flattened.ContainsKey("value.wildcard.one.renamed"));
-            
-            Assert.False(flattened.ContainsKey("start.one.end"));
-            Assert.False(flattened.ContainsKey("start.two.end"));
-            Assert.True(flattened.ContainsKey("value.renamed.one.renamed"));
-            Assert.True(flattened.ContainsKey("value.renamed.two.renamed"));
-        }
-        
-        [Fact]
-        public async Task Apply_Ignore_WhenValue_IsNull()
-        {
-            var (skip, flattened) = await _fieldRenamer.Apply(
-                new Dictionary<string, object>
-                    {{"key1", null}},
-                @"maps: new Dictionary<string, string>()
-                {
-                    {""key1"", ""newKey""}
-                }");
-            Assert.False(skip);
-            Assert.Null(flattened["key1"]);
-        }
     }
 }
