48a5248fada32a6e18c00aa30c4d58de71da0fde
Picardo, Roshan Silvan <Roshan.Picardo@cba.com.au>
2023-04-14 09:54:20 +1000

    Remove Kafka dependency from Serialiser (Plugin)

--- src/Kafka.Connect.Plugin/Serializers/Serializer.cs
+++ src/Kafka.Connect.Plugin/Serializers/Serializer.cs
@@ -1,12 +1,12 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Confluent.Kafka;
 using Newtonsoft.Json.Linq;
 
 namespace Kafka.Connect.Plugin.Serializers
 {
     public abstract class Serializer : ISerializer
     {
-        public abstract Task<byte[]> Serialize<T>(JToken data, SerializationContext context, T schema);
+        public abstract Task<byte[]> Serialize<T>(JToken data, string topic, IDictionary<string, byte[]> headers, T schema, bool isValue = true);
 
         public bool IsOfType(string type)
         {
